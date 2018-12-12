fddb4c27acaaefceecafa824fc55894a2c51ed3a
csvinhal <cristiano.vinhal10@gmail.com>
2018-12-11 23:00:46 -0200

    sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Aux from "../../hoc/ReactAux";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from "./Layout.css";
 
 const layout = propos => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{propos.children}</main>
   </Aux>
 );
